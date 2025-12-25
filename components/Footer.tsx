b69b333e4cc592ef1ff96b29352c2c1735b0a43b
LETHEARS <166290815+LETHEARS@users.noreply.github.com>
2025-12-25 22:10:58 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="text-zinc-400 text-center py-4">
       <div className="container mx-auto">
-        <p className="text-sm">Made by <span className="text-white underline"><a href="https://parsher.xyz">Parsher</a></span> with ❤️</p>
+        <p className="text-sm">Made by <span className="text-white underline"><a href="https://kaptankral.vercel.app">Kaptan Kral</a></span> with ❤️</p>
       </div>
     </footer>
   );
