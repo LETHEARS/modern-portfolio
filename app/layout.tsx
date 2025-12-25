72426ab1ce3a7f13db88b1f98d64a0e696bf8774
LETHEARS <166290815+LETHEARS@users.noreply.github.com>
2025-12-25 21:57:11 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Parsher',
-  description: ' Hi, I’m @parsher I\'m currently learning I am currently living in Turkey.',
+  title: 'Kaptan Kral',
+  description: ' Merhaba, Ben Kaptan Kral Selamün Aleyküm.',
   themeColor: '#8B5CF6',
   robots: 'index, follow',
   viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
