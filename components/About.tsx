76791c2dee91ab32efcde4e8de27c80871d9ec38
LETHEARS <166290815+LETHEARS@users.noreply.github.com>
2025-12-25 22:39:53 +0300

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { FaClock, FaEnvelope, FaCloud, FaMapMarkerAlt, FaUser, FaBirthdayCake, FaHiking, FaPalette, FaYoutube, FaFlag } from 'react-icons/fa';
 import Image from 'next/image';
 
-function IstanbulTime() {
+function AntalyaTime() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
@@ -13,14 +13,14 @@ function IstanbulTime() {
     return () => clearInterval(timer);
   }, []);
 
-  const istanbulTime = new Date(time.toLocaleString('en-US', { timeZone: 'Europe/Istanbul' }));
-  const formattedTime = istanbulTime.toLocaleTimeString();
-  const formattedDate = istanbulTime.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+  const AntalyaTime = new Date(time.toLocaleString('en-US', { timeZone: 'Europe/Antalya' }));
+  const formattedTime = AntalyaTime.toLocaleTimeString();
+  const formattedDate = AntalyaTime.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
 
   return (
     <p className="flex items-center">
       <FaClock className="mr-2" />
-      Time in Istanbul: {formattedDate}・{formattedTime}
+      Time in Antalya: {formattedDate}・{formattedTime}
     </p>
   );
 }
@@ -31,12 +31,12 @@ export default function About() {
     <div className="container mx-auto bg-zinc-900/30 w-11/12 sm:w-9/12 md:w-7/12 rounded-md flex flex-col-reverse my-16 py-10 px-8 justify-between md:flex-row md:items-center">
       <div className="md:w-8/12">
         <div className="space-y-2">
-          <h1 className="text-2xl font-semibold" id="about">About Me</h1>
+          <h1 className="text-2xl font-semibold" id="about">Benim Hakkımda</h1>
           <p>
             Merhaba Ben Kaptan Kral Selamünaleyküm Nider Nişlersiniz Sağolun Bende iyiyim Derslerinize Çalışın olurmu hadi iyi günler.
           </p>
           <br />
-          <IstanbulTime />
+          <AntalyaTime />
           <p className="flex items-center">
             <FaEnvelope className="mr-2" />
             Mail :
